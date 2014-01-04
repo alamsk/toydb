30eac8f0a24c3c59bd7642bb47b24ee8c5d5fa0d
Biswarup Adhikari <biswarupadhikari@gmail.com>
2014-01-05 02:06:39 +0530

    Cache Folder And Databases Folder Moved inside Storage Folder

--- src/BiswarupAdhikari/ToyDB/Config/Config.php
+++ src/BiswarupAdhikari/ToyDB/Config/Config.php
@@ -3,8 +3,8 @@ namespace BiswarupAdhikari\ToyDB\Config;
 class Config
 {
 	function __construct(){
-		$this->dbPath=__DIR__.'/../databases';
-		$this->cacheDir=__DIR__.'/../Cache';
+		$this->dbPath=__DIR__.'/../Storage/Databases';
+		$this->cacheDir=__DIR__.'/../Storage/cache';
 	}
 	public $username="root";
 	public $password="123456";
